da295c385e528535481f1ec6b458ca19affc1e27
luizgasparetto <luhmgasparetto@gmail.com>
2023-02-18 16:43:51 -0300

    final

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,34 +3,34 @@ import './NavBar.css';
 import { AiOutlineMenu } from 'react-icons/ai'
 
 function NavBar() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleClick = () => {
-    setMenuOpen(!menuOpen);
+    setShowMenu(!showMenu);
   }
 
   return (
     <nav className="navbar">
       <h1 className='logo'>LOGO</h1>
-      <div className="navbar-center">
+      <div className="menu-dropdown" style={{ display: showMenu ? 'block' : 'none' }}>
         <a href="#">Home</a>
         <a href="#">Services</a>
         <a href="#">Portfolio</a>
         <a href="#">About us</a>
         <a href="#">Contact</a>
       </div>
+      <div className="navbar-center" onClick={() => setShowMenu(false)}>
+        <a href="#">Home</a>
+        <a href="#">Services</a>
+        <a href="#">Portfolio</a>
+        <a href="#">About us</a>
+        <a href="#">Contact</a>
+      </div>
+      <AiOutlineMenu className='menu-icon' onClick={() => setShowMenu(!showMenu)} />
       <div className="navbar-right">
         <button>Sign In</button>
         <button>Sign Up</button>
       </div>
-      <div className="menu-dropdown" style={{ display: menuOpen ? 'block' : 'none' }}>
-        <a href="#">Home</a>
-        <a href="#">Services</a>
-        <a href="#">Portfolio</a>
-        <a href="#">About us</a>
-        <a href="#">Contact</a>
-      </div>
-      <AiOutlineMenu className='menu-icon' onClick={handleClick} />
     </nav>
   );
 }
