b78ee653195280bbf852e61c25fc97567491a708
luizgasparetto <luhmgasparetto@gmail.com>
2023-02-18 13:28:53 -0300

    final

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
 import { AiOutlineMenu } from 'react-icons/ai'
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <nav className="navbar">
       <h1 className='logo'>LOGO</h1>
@@ -17,9 +23,16 @@ function NavBar() {
         <button>Sign In</button>
         <button>Sign Up</button>
       </div>
-      <AiOutlineMenu className='invisivel' />
+      <div className="menu-dropdown" style={{ display: menuOpen ? 'block' : 'none' }}>
+        <a href="#">Home</a>
+        <a href="#">Services</a>
+        <a href="#">Portfolio</a>
+        <a href="#">About us</a>
+        <a href="#">Contact</a>
+      </div>
+      <AiOutlineMenu className='menu-icon' onClick={handleClick} />
     </nav>
   );
 }
 
-export default NavBar
+export default NavBar;
