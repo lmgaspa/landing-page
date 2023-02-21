4726523dfe03759b542220761a5ff4802633c962
luizgasparetto <luhmgasparetto@gmail.com>
2023-02-21 16:57:46 -0300

    final

--- src/NavBar.js
+++ src/NavBar.js
@@ -12,8 +12,7 @@ function NavBar() {
   return (
     <nav className="navbar">
       <h1 className='logo'>LOGO</h1>
-      <div className="menu-dropdown" style={{ display: showMenu ? 'block' : 'none' }}>
-        <a href="#">Home</a>
+      <div className={"menu-dropdown " + (showMenu ? "visible" : "invisible")}>        <a href="#">Home</a>
         <a href="#">Services</a>
         <a href="#">Portfolio</a>
         <a href="#">About us</a>
